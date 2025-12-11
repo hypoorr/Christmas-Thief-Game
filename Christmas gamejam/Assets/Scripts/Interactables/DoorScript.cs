5586bae9de52efa295c45680424157a681f2d0ed
hypoorr <ollie.heron@gmail.com>
2025-12-11 21:39:22 +0000

    menu screen + other stuff

--- Christmas gamejam/Assets/Scripts/Interactables/DoorScript.cs
+++ Christmas gamejam/Assets/Scripts/Interactables/DoorScript.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
- 
+
 public class DoorScript : Interactable
 {
     [SerializeField] private GameObject playerObject;
     [SerializeField] private Transform destinationPoint;
- 
+
     protected override void Interact()
     {
         Debug.Log("Interacted with " + gameObject.name);
+        CharacterController cc = playerObject.GetComponent<CharacterController>();
+
+        cc.enabled = false;
         // Move the player to the destination
         playerObject.transform.position = destinationPoint.position;
+
+        cc.enabled = true;
     }
 }
